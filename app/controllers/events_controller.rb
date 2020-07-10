8d227182cfb60d640664ed6c49a2064fd87d840d
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-10 12:45:49 +0900

    update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,5 +3,15 @@ class EventsController < ApplicationController
   end
 
   def new
+    @event = Event.new
+  end
+
+  def create
+    Event.create(event_params)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:title, :datetime, :image, :content, :tag)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
-  resources :events, only: [:index, :new]
+  resources :events, only: [:index, :new, :create]
   resources :users, only: [:edit, :update]
 end
