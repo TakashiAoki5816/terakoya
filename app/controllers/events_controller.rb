c8f749e3874b1b67ef1334d131c7ad438bd7358d
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-14 17:54:56 +0900

    update uploader

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -17,6 +17,10 @@ class EventsController < ApplicationController
     end
   end
 
+  def show
+    @event = Event.find(params[:id])
+  end
+
   private
   def event_params
     params.require(:event).permit(:name, :title, :datetime, :image, :content, :tag)
