332e4b998051320cc3b1daf18ce9b4863d280b18
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-14 17:54:36 +0900

    event show

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'events#index'
-  resources :events, only: [:index, :new, :create]
+  resources :events, only: [:index, :new, :create, :show]
   resources :users, only: [:edit, :update]
 end
