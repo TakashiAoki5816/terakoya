6c1e3c144a5f117e73b1de82bf69fa73dedbb62e
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-18 02:49:12 +0900

    end simple login

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,10 @@ class User < ApplicationRecord
   has_many :tickets
 
   validates :name, presence: true, uniqueness: true
+
+  def self.guest
+    find_or_create_by!(name: 'ゲスト', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
