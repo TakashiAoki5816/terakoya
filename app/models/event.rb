3d4ec05d31a2878316f2e8b658cdbb9150465883
Takashi Aoki <arfaben_5816@icloud.com>
2020-07-18 01:02:32 +0900

    devise update

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
-  has_many :event_users
-  has_many :users, through: :event_users
+  has_many :event_users, dependent: :destroy
+  has_many :users, through: :event_users, dependent: :destroy
   has_many :tickets
 
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :event_users
-  has_many :events, through: :event_users
+  has_many :event_users, dependent: :destroy
+  has_many :events, through: :event_users, dependent: :destroy
   has_many :tickets
 
   validates :name, presence: true, uniqueness: true
